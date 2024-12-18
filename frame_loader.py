afcba7b0681031a92aa1b9d90e5d00ccb5724194
Ryan Gelly <rjtgelly@gmail.com>
2024-12-17 23:17:36 -0500

    added multiprocess

--- frame_loader.py
+++ frame_loader.py
@@ -4,29 +4,62 @@ import rawpy
 import numpy as np
 import imageio
 
-directory = 'test_images'
+import multiprocessing
 
-rgb = None
-
-for fn in os.listdir(directory):
+def worker_function(fn):
+    # Your function to be executed in parallel
+    directory = 'test_images'
     fp = os.path.join(directory,fn)
     if os.path.isfile(fp):
         print(fn)
         raw = rawpy.imread(fp)
-        if rgb is None:
-            rgb = np.array(raw.postprocess(output_bps=16))
-            average = np.zeros(rgb.shape, np.float32)
-            average += rgb
-        else:
-            rgb = np.array(raw.postprocess(output_bps=16))
-            average += rgb
+        return np.array(raw.postprocess(output_bps=16))
+
+
+if __name__ == '__main__':
+    directory = 'test_images'
+    dirlist = os.listdir(directory)
+
+    fn = dirlist[0]
+    fp = os.path.join(directory,fn)
+    if os.path.isfile(fp):
+        print(fn)
+        raw = rawpy.imread(fp)
+
+    rgb = np.array(raw.postprocess(output_bps=16))
+    average = np.zeros(rgb.shape, np.float32)
+
+    with multiprocessing.Pool(processes=2) as pool: 
+        results = pool.map(worker_function, dirlist)
+
+    for result in results:
+        average += result
+
+    print(np.max(average))
+
+    maxval = np.max(average)
+    minval = np.min(average)
+    print(maxval, minval)
+    average = np.array(2**15 * (average-minval)/(maxval-minval), np.uint16)
     print(average)
 
-print(np.max(average))
+# rgb = None
 
-maxval = np.max(average)
-minval = np.min(average)
-print(maxval, minval)
-average = np.array(2**15 * (average-minval)/(maxval-minval), np.uint16)
-print(average)
-imageio.imsave('default.tiff', average)
+
+#         if rgb is None:
+#             rgb = np.array(raw.postprocess(output_bps=16))
+#             average = np.zeros(rgb.shape, np.float32)
+#             average += rgb
+#         else:
+#             rgb = np.array(raw.postprocess(output_bps=16))
+#             average += rgb
+#     print(average)
+
+# print(np.max(average))
+
+# maxval = np.max(average)
+# minval = np.min(average)
+# print(maxval, minval)
+# average = np.array(2**15 * (average-minval)/(maxval-minval), np.uint16)
+# print(average)
+# imageio.imsave('default.tiff', average)
